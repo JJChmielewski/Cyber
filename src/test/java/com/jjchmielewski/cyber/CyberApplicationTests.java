e3387b09b916306432db7bd203b536d6197ccdca
Jakub Chmielewski <j.j.chmielewski@wp.pl>
2023-11-20 16:38:52 +0100

    Initial commit

---
+++ src/test/java/com/jjchmielewski/cyber/CyberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjchmielewski.cyber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
